c293fdf6ea586511273585edbbd746fc43a0ee64
Segelzwerg <marcel.haas@hhu.de>
2020-03-17 17:28:02 +0100

    ADD: custom exceptions

--- src/drone_controller/exception/exceptions.py
+++ src/drone_controller/exception/exceptions.py
@@ -2,3 +2,13 @@ class DroneControllerError(Exception):
     """
     Base class for exception in this module.
     """
+
+
+class UserInputError(DroneControllerError):
+    """
+    Incorrect user input.
+    """
+
+    def __init__(self, expression, message):
+        self.expression = expression
+        self.message = message
