c467dfab9a17f9a4fe81329ee012da141dbcdcc8
Segelzwerg <marcel.haas@hhu.de>
2020-03-17 20:26:45 +0100

    ADD: drone state implementation

---
+++ src/drone_controller/input_layer/drone_state.py
@@ -0,0 +1,34 @@
+class DroneState:
+    def __init__(self, position, velocity, velocity_ang, acceleration, acceleration_ang):
+        self._position = position
+        self._velocity = velocity
+        self._velocity_ang = velocity_ang
+        self._acceleration = acceleration
+        self._acceleration_ang = acceleration_ang
+
+        @property
+        def state(self):
+            """
+            Maps the drone state to a dictionary.
+            :param self: this drone state
+            :return: dictionary
+            """
+            return {
+                "Position": self._position,
+                "Velocity": self._velocity,
+                "Angular Velocity": self._velocity_ang,
+                "Acceleration": self._acceleration,
+                "Angular Acceleration": self._acceleration_ang
+            }
+
+    @classmethod
+    def create_three_dimensional_position(cls, position, velocity, velocity_ang, acceleration, acceleration_ang):
+        """
+        Uses a three dimensional position vector, containing x,y,z coordinates.
+        :param position: 3D vector of the drone's position
+        :param velocity: 3D vector of the drone's translation velocity
+        :param velocity_ang: 3D vector of the drone's angular velocity
+        :param acceleration: 3D vector of the drone's translation acceleration
+        :param acceleration_ang: 3D vector of the drone's angular acceleration
+        """
+        cls(position, velocity, velocity_ang, acceleration, acceleration_ang)
