8f8e00a108d13e44b1d8f7b28746d95a0884936c
Segelzwerg <marcel.haas@hhu.de>
2020-03-17 17:31:05 +0100

    ADD: thrust calculator skeleton

---
+++ src/drone_controller/calculation_layer/thrust_calculator.py
@@ -0,0 +1,19 @@
+class ThrustCalculator:
+    """
+    Calculator for rotor thrusts.
+    """
+
+    def __init__(self, drone_state, expected_state):
+        """
+        :param drone_state: the momentary drone state
+        :param expected_state: the expected state of the dron
+        """
+        self.drone_state = drone_state
+        self.expected_state = expected_state
+
+    def calc(self, rotor_count):
+        """
+        Calculates the thrust.
+        :param rotor_count: the number of rotors the calculator should calculate.
+        """
+        pass
