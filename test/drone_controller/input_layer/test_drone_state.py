b002c8b791b91fc6b635328bb3aa474945467283
Segelzwerg <marcel.haas@hhu.de>
2020-03-17 20:44:56 +0100

    ADD: tet for drone state

---
+++ test/drone_controller/input_layer/test_drone_state.py
@@ -0,0 +1,22 @@
+from src.drone_controller.input_layer.drone_state import DroneState
+
+
+def test_create_three_dimensional_position():
+    position = [1, 1, 1]
+    velocity = [1, 1, 1]
+    velocity_ang = [1, 1, 1]
+    acceleration = [1, 1, 1]
+    acceleration_ang = [1, 1, 1]
+    drone_state = DroneState.create(position, velocity, velocity_ang, acceleration, acceleration_ang)
+
+    expected_dict = {
+        "Position": position,
+        "Velocity": velocity,
+        "Angular Velocity": velocity_ang,
+        "Acceleration": acceleration,
+        "Angular Acceleration": acceleration_ang
+    }
+
+    state_dict = drone_state.state
+
+    assert state_dict == expected_dict
