53b0247e3e5bd6c40b4ba597199f5412bee26998
Segelzwerg <marcel.haas@hhu.de>
2020-03-17 19:19:16 +0100

    UPDATE: disable pylint warning until class is implemented

--- src/drone_controller/input_layer/drone_state.py
+++ src/drone_controller/input_layer/drone_state.py
@@ -1,3 +1,5 @@
+# pylint: disable=too-few-public-methods
+# keep this only until this is implemented
 class DroneState:
     """
     Wrapper for drone state.
--- src/drone_controller/input_layer/drone_state_mapper.py
+++ src/drone_controller/input_layer/drone_state_mapper.py
@@ -3,6 +3,8 @@ Mapper Module
 """
 
 
+# pylint: disable=too-few-public-methods
+# keep this only until this is implemented
 class DroneStateMapper:
     """
     Maps different inputs to an expected drone state.
