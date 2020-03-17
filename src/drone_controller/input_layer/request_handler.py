b2a13aee580270d28741784bcf229d263cf0ffe6
Segelzwerg <marcel.haas@hhu.de>
2020-03-17 17:23:49 +0100

    ADD: request handling for keyboard input

---
+++ src/drone_controller/input_layer/request_handler.py
@@ -0,0 +1,41 @@
+"""
+This module handles all requests. This will cover REST calls as well as native python calls.
+"""
+from src.drone_controller.calculation_layer.thrust_calculator import ThrustCalculator
+from src.drone_controller.exception.exceptions import UserInputError
+from src.drone_controller.input_layer.drone_state import DroneState
+from src.drone_controller.input_layer.drone_state_mapper import DroneStateMapper
+
+
+class RequestHandler:
+    """
+    Handles request from python code.
+    """
+
+    def __init__(self, drone_state, expected_state):
+        self._thrust_calc = ThrustCalculator(drone_state, expected_state)
+
+    @classmethod
+    def keyboard_input(cls, drone_state: DroneState, user_input: dict):
+        """
+        Handles user input and will call the thrust calculator.
+        :param drone_state: the momentary state of the drone
+        :param user_input: keyboard input of the user
+        """
+        if not all(key in user_input for key in ["Rotation Forward",
+                                                 "Rotation Right",
+                                                 "Rotation Backward",
+                                                 "Rotation Left",
+                                                 "Acceleration"]):
+            raise UserInputError(user_input, "The keyboard input is complete.")
+
+        state_mapper = DroneStateMapper()
+        expected_state = state_mapper.keyboard(user_input)
+        cls(drone_state, expected_state)
+
+    def calc_quadro(self):
+        """
+        Calls to the thrust calculator to compute the thrust for four rotors.
+        :return: thrusts for four rotor
+        """
+        return self._thrust_calc.calc(4)
