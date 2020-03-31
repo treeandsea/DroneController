1e1e206ddc63a98171289bd2760c37937f4fdf5d
Segelzwerg <marcel.haas@hhu.de>
2020-03-31 12:38:15 +0200

    UPDATE: use unittest

--- test/drone_controller/input_layer/test_drone_state_mapper.py
+++ test/drone_controller/input_layer/test_drone_state_mapper.py
@@ -1,31 +1,34 @@
+from unittest import TestCase
+
 from src.drone_controller.input_layer.drone_state import DroneState
 from src.drone_controller.input_layer.drone_state_mapper import DroneStateMapper
 
 
-def test_keyboard_simple_up():
-    position = [0, 0, 1]
-    rotation = [0, 0, 0]
-    velocity = [0, 0, 1]
-    velocity_ang = [0, 0, 0]
-    acceleration = [0, 0, 1]
-    acceleration_ang = [0, 0, 0]
+class TestDroneStateMapper(TestCase):
+    def test_keyboard_simple_up(self):
+        position = [0, 0, 1]
+        rotation = [0, 0, 0]
+        velocity = [0, 0, 1]
+        velocity_ang = [0, 0, 0]
+        acceleration = [0, 0, 1]
+        acceleration_ang = [0, 0, 0]
 
-    expected_state = DroneState(position, rotation, velocity, velocity_ang, acceleration,
-                                acceleration_ang)
+        expected_state = DroneState(position, rotation, velocity, velocity_ang, acceleration,
+                                    acceleration_ang)
 
-    position = [0, 0, 0]
-    velocity = [0, 0, 0]
-    acceleration = [0, 0, 0]
-    drone_state = DroneState(position, rotation, velocity, velocity_ang, acceleration,
-                             acceleration_ang)
+        position = [0, 0, 0]
+        velocity = [0, 0, 0]
+        acceleration = [0, 0, 0]
+        drone_state = DroneState(position, rotation, velocity, velocity_ang, acceleration,
+                                 acceleration_ang)
 
-    user_input = {"Rotation Forward": 0,
-                  'Rotation Right': 0,
-                  "Rotation Backward": 0,
-                  "Rotation Left": 0,
-                  "Acceleration": 1}
+        user_input = {"Rotation Forward": 0,
+                      'Rotation Right': 0,
+                      "Rotation Backward": 0,
+                      "Rotation Left": 0,
+                      "Acceleration": 1}
 
-    mapper = DroneStateMapper()
-    future_state: DroneState = mapper.keyboard(drone_state, user_input)
+        mapper = DroneStateMapper()
+        future_state: DroneState = mapper.keyboard(drone_state, user_input)
 
-    assert future_state == expected_state
+        self.assertEqual(future_state, expected_state)
