f4a09c1f994f1fc5836545b37d2085e7c80b94d2
Segelzwerg <marcel.haas@hhu.de>
2020-04-04 14:11:09 +0200

    UPDATE: simple up calculation possible
    Signed-off-by: Segelzwerg <marcel.haas@hhu.de>

--- src/drone_controller/calculation_layer/ThrustCalculator.py
+++ src/drone_controller/calculation_layer/ThrustCalculator.py
@@ -1,6 +1,10 @@
 import abc
 
+import numpy
+
 from src.drone_controller.input_layer.drone_state import DroneState
+
+GRAVITATIONAL_ACCELERATION = 9.81
 
 
 class ThrustCalculator(metaclass=abc.ABCMeta):
@@ -15,4 +19,20 @@ class ThrustCalculatorQuadroCopter(ThrustCalculator):
         self.rotor_thrust = rotor_thrust
 
     def calc(self, current_state: DroneState, future_state: DroneState) -> list:
-        pass
+        jerk = self.subtract_lists(future_state.state_dict['Acceleration'],
+                                   current_state.state_dict[
+                                       'Acceleration'])
+
+        needed_acceleration = jerk[0:2]
+        needed_acceleration.append(jerk[2] + GRAVITATIONAL_ACCELERATION)
+        force = [x * self.weight for x in needed_acceleration]
+
+        return [(numpy.linalg.norm(force) / 4) / self.rotor_thrust] * 4
+
+    @staticmethod
+    def subtract_lists(first_list, second_list):
+        return [x - y for x, y in zip(first_list, second_list)]
+
+    @staticmethod
+    def add_lists(first, second):
+        return [sum(x) for x in zip(first, second)]
