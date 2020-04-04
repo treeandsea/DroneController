9e3f568de3c73df8503b86ed9d605e9dfe57cd27
Segelzwerg <marcel.haas@hhu.de>
2020-04-04 23:09:22 +0200

    REFACTOR: use variable instead of multiple method calls
    Signed-off-by: Segelzwerg <marcel.haas@hhu.de>

--- src/drone_controller/input_layer/drone_metrics.py
+++ src/drone_controller/input_layer/drone_metrics.py
@@ -20,12 +20,14 @@ class DroneMetrics:
     def __eq__(self, other, float_precision=0.001):
         if not isinstance(other, DroneMetrics):
             raise TypeError(f'other is not DroneMetrics but {type(other)}.')
-        if len(self.__dict__()) != len(other.__dict__()):
+        metrics_dict = self.__dict__()
+        other_dict = other.__dict__()
+        if len(metrics_dict) != len(other_dict):
             return False
 
-        for (keys, metric_values, other_values) in zip(self.__dict__().keys(),
-                                                       self.__dict__().values(),
-                                                       other.__dict__().values()):
+        for (keys, metric_values, other_values) in zip(metrics_dict.keys(),
+                                                       metrics_dict.values(),
+                                                       other_dict.values()):
 
             if math.fabs(metric_values - other_values) > float_precision:
                 print(f'Unequal at {keys} {metric_values} != {other_values}')
