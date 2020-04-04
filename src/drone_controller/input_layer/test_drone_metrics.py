63bf9ed0e6a7c569550cba5c02842fa2dbebcb9c
Segelzwerg <marcel.haas@hhu.de>
2020-04-04 22:17:48 +0200

    ADD: test for from dict build
    Signed-off-by: Segelzwerg <marcel.haas@hhu.de>

--- src/drone_controller/input_layer/test_drone_metrics.py
+++ src/drone_controller/input_layer/test_drone_metrics.py
@@ -8,20 +8,28 @@ class DroneMetricTest(TestCase):
         """
         Sets up some drone metrics.
         """
+
         self.mass = 2.56
         self.radius = 13.4
         self.rotor_count = 8
         self.thrust_per_rotor = 43.2
 
-        self.metrics = DroneMetrics(self.mass, self.thrust_per_rotor, self.rotor_count, self.radius)
+        self.metrics_dict = {'mass': self.mass,
+                             'thrust_per_rotor': self.thrust_per_rotor,
+                             'rotor_count': self.rotor_count,
+                             'radius': self.radius}
+
+        self.metrics = DroneMetrics(mass=self.mass,
+                                    thrust_per_rotor=self.thrust_per_rotor,
+                                    rotor_count=self.rotor_count,
+                                    radius=self.radius)
 
     def test_metric_dict(self):
         """
         Test if the dictionary is correctly built.
         """
-        metrics_dict = {'mass': self.mass,
-                        'thrust_per_rotor': self.thrust_per_rotor,
-                        'rotor_count': self.rotor_count,
-                        'radius': self.radius}
 
-        self.assertEqual(self.metrics.metric_dict, metrics_dict)
+        self.assertEqual(self.metrics.metric_dict, self.metrics_dict)
+
+    def test_metrics_from_dict(self):
+        self.assertEqual(self.metrics, DroneMetrics())
