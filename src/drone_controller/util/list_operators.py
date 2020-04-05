30edab2ef752b129a0fcba4ee404191681741df8
Segelzwerg <marcel.haas@hhu.de>
2020-04-05 18:36:32 +0200

    REFACTOR: move add list to package
    Signed-off-by: Segelzwerg <marcel.haas@hhu.de>

--- src/drone_controller/util/list_operators.py
+++ src/drone_controller/util/list_operators.py
@@ -1,3 +1,13 @@
+def add_lists(first_list, second_list):
+    """
+    Adds two lists element wise
+    :param first_list:
+    :param second_list:
+    :return:
+    """
+    return [sum(x) for x in zip(first_list, second_list)]
+
+
 def subtract_lists(first_list, second_list):
     """
     Subtracts two lists
