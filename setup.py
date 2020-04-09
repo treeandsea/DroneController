150c5a3b28c93deee884362300986613591caca1
Marcel <marcel.haas@hhu.de>
2020-04-09 11:35:43 +0200

    UPDATE: version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='DroneController',
-    version='0.1.0',
+    version='1.0.0',
     packages=['src', 'src.drone_controller', 'test'],
     url='https://www.github.com/treeandsea/DroneController',
     license='GPL-3.0',
